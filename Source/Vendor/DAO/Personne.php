186a1b01538e43e5396a55e7425eeb53e12c3ec1
jonathan merandat <jonathan.merandat@outlook.com>
2018-09-27 16:22:59 +0200

    ajout de l'objet personne + initialisation BD simple

---
+++ Source/Vendor/DAO/Personne.php
@@ -0,0 +1,21 @@
+<?php
+
+class Personne{
+
+    var $nom;
+    var $prenom;
+    var $nombreCroissantAmene;
+
+    function __construct($nom, $prenom){
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->nombreCroissantAmene = 0;
+    }
+
+
+
+}
+
+
+
+?>
