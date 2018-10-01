32509e58d88c386c441e46fff1e3452b5dbf0090
Kranck <frizzy.rastay@gmail.com>
2018-10-01 12:25:30 +0200

    Work on sending mails objects from mailsender

--- Source/Vendor/Mailing/MailSender.php
+++ Source/Vendor/Mailing/MailSender.php
@@ -8,42 +8,63 @@ namespace Mailing;
     require 'Lib/Exception.php';
     require 'Lib/PHPMailer.php';
     require 'Lib/SMTP.php';
+    require 'Mail.php';
     
 
     class MailSender
     {
 
-        var $m_sMailSender = "m2test2.croissant.show@gmail.com";
+        var $m_sLogin;  //< The login of the mail sender (from)
+        var $m_sPassword;   //< The password of the mail sender (from)
 
-        function __construct()
+        //
+        var $m_oMailer;
+
+        public function __construct($sLogin , $sPassword)
         {
-        }
+            $this->m_sLogin = $sLogin;
+            $this->m_sPassword = $sPassword;
 
-        function getSenderMail()
-        {
-            return $this->m_sSenderMail;
+            //Constructs the mailer object
+            echo $this->m_sLogin;
+            echo $this->m_sPassword;
+            $this->m_oMailer = new PHPMailer();
+            $this->configureSMTP();
         }
-
-        function sendMail($sDestination)
-        {
-            
-        }
-
 
         private function configureSMTP()
         {
-            
+            $this->m_oMailer->SMTPDebug = 2;            //< Enable verbose
+            $this->m_oMailer->isSMTP();                 //< Use SMTP
+            $this->m_oMailer->Host = "smtp.gmail.com";  //< Specify the SMTP
+            $this->m_oMailer->SMTPAuth = true;          //< Enable the authentication of SMTP
+            $this->m_oMailer->Username = $m_sLogin;     //< Login
+            $this->m_oMailer->Password = $m_sPassword;  //< Password
+            $this->m_oMailer->SMTPSecure = "ssl";       //< ssl encyption is neede for gmail
+            $this->m_oMailer->Port = 465;               //< Gmail port       
         }
+
+        public function sendMail($oMail)
+        {
+            //Configure last infos of the mail
+            $this->m_oMailer->setFrom($oMail->getSender());
+            $this->m_oMailer->addAddress($oMail->getRecipient());
+            $this->m_oMailer->Subject   = $oMail->getSubject();
+            $this->m_oMailer->Body      = $oMail->getBody();
+            echo $this->m_oMailer->send();
+        }
+
     }
 
-    $ms = new MailSender();
+    // $ms = new MailSender("m2test2.croissant.show@gmail.com" , "pas2pitiepourlescroissants!");
+    // $ms->sendMail(new Mail("m2test2.croissant.show@gmail.com" , "yannis.beaux@gmail.com" , "Je suis un mail" , "Je suis un corps"));
 
 
     $mail = new PHPMailer(true);
         //Server settings
         $mail->SMTPDebug = 2;                                 // Enable verbose debug output
         $mail->isSMTP();                                      // Set mailer to use SMTP
-        $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+        $mail->Host = 'smtp.gmail.com';                         // Specify main and backup SMTP servers
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
         $mail->Username = 'm2test2.croissant.show@gmail.com';                 // SMTP username
         $mail->Password = 'pas2pitiepourlescroissants!';                           // SMTP password
