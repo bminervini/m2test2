d7cea65205d2219fa66ab0f089a1d271a27f4812
Kranck <frizzy.rastay@gmail.com>
2018-10-04 17:17:10 +0200

    Started rediraction for dashboard pages

--- static/dashboard.php
+++ static/dashboard.php
@@ -7,6 +7,26 @@
         <?php include 'jumbotron.php';?>
         
         <?php include 'navbar.php';?>
+
+        
+        <div class="container">
+            
+            <div class="row">
+                
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                </div>
+
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 text-center">
+                    <h1>Bienvenue sur le dashboard de Croissant Show'</h1>
+                </div>
+
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                </div>
+                
+            </div>
+            
+        </div>
+        
         
         <?php include 'bottom.php';?>
 
