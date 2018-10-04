74ad161c5556c0a0b7d5d967540942d3261d94fb
Kranck <frizzy.rastay@gmail.com>
2018-10-04 15:42:03 +0200

    Small changes on navbar

--- static/navbar.php
+++ static/navbar.php
@@ -1,14 +1,10 @@
-<nav class="navbar navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Dashboard</a>
-    <ul class="nav navbar-nav">
-        <li class="active">
-            <a href="#" style="color:white">Calendrier</a>
-        </li>
-        <li>
-            <a href="#" style="color:white">Participants</a>
-        </li>
-        <li>
-            <a href="#" style="color:white">Parametres</a>
-        </li>
-    </ul>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="nav navbar">
+        <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="#" style="color:white">Calendrier</a>
+            <a class="nav-item nav-link" href="#" style="color:white">Participants</a>
+            <a class="nav-item nav-link" href="#" style="color:white">Parametres</a>
+            <a class="nav-item nav-link" href="#"><img src="i/shutdown.png" width="2%"></img></a>
+        </div>
+    </div>
 </nav>
