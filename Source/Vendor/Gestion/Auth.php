08ed96e9a8e14e21517ae0792a24a6df7d04a573
brchenu <brandon.chenu@protonmail.com>
2018-10-09 14:23:00 +0200

    modif in auth class

--- Source/Vendor/Gestion/Auth.php
+++ Source/Vendor/Gestion/Auth.php
@@ -7,7 +7,7 @@ namespace Auth;
     class Auth{
         
         private $dao;
-
+        
         public function __construct(){
             $this->dao = new DAO();
         }
@@ -19,18 +19,15 @@ namespace Auth;
                 $cryptedPassword = md5($password);
                 $query = "SELECT * FROM personne WHERE username='$username' AND password='$cryptedPassword'LIMIT 1";
         
-                $req = $this->dao->querySQL($query);
-                
+                $req = $this->dao->querySQL($query);                
                 $result = $req->fetch();
  
                 if($result){
-                    session_start();
-                    $_SESSION['username'] = $username;
-                    $_SESSION['password'] = $cryptedPassword;
-
-                   header("Location: dashboard.php");
+                    
+                    $this->createSession($username, $cryptedPassword, $result['isAdmin']);
+                    header("Location: dashboard.php");
                    
-                   echo "Connecté !";
+                    echo "Connecté !";
                 }else{
                     echo "Mauvais mdp !";
                     $error = "Your Login Name or Password is invalid";
@@ -39,13 +36,28 @@ namespace Auth;
         }
 
         public static function isLogged(){
-            if(isset($_SESSION['username']) and isset($_SESSION['password'])){
+            session_start();//> START SESSION 
+            if(isset($_SESSION['username']) and isset($_SESSION['password']) and isset($_SESSION['isAdmin'])){
                 //Later retrieve the user from the database to perform a verification
                 return true;
             }
             return false;
         }
-    }
 
+        public static function Logout(){
+            if($this->isLogged()){
+                unset($_SESSION["username"]);
+                unset($_SESSION['password']); 
+                unset($_SESSION['isAdmin']); 
+            }
+        }
+
+        public static function createSession($username, $password, $isAdmin){
+            session_start();
+            $_SESSION['username'] = $username;
+            $_SESSION['password'] = $password;
+            $_SESSION['isAdmin'] = $isAdmin
+        }
+    }
 
 ?>
