039331c6af5d3be68a4d8e5d2aa334e77ca20c02
Bminervini <baptiste.minervini@outlook.fr>
2018-10-11 14:46:29 +0200

    wrong path authentication

--- src/main/Vendor/Gestion/Registration.php
+++ src/main/Vendor/Gestion/Registration.php
@@ -2,10 +2,10 @@
 
 
 namespace Vendor\Gestion;
-    require_once("../../htdocs/m2test2/src/main/Vendor/DAO/DAO.php"); // use for Atoum
+    require_once("../src/main/Vendor/DAO/DAO.php"); // use for Atoum
     //require_once("../src/main/Vendor/DAO/DAO.php");   
     use \DAO\DAO;
-    require_once("../../htdocs/m2test2/src/main/Vendor/Models/Personne.php");// use for Atoum
+    require_once("../src/main/Vendor/Models/Personne.php");// use for Atoum
     //require_once("../src/main/Vendor/Models/Personne.php");
     use \Vendor\Models\Personne;
 
