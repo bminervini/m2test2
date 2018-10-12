63d2662b2db85211f6f250242a9a2fa2a8f1c37a
Bminervini <baptiste.minervini@outlook.fr>
2018-10-12 21:18:29 +0200

    add test selenium working on local and fix for some merge waste

---
+++ src/test/java/localHostSeleniumTest.java
@@ -0,0 +1,76 @@
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class localHostSeleniumTest {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseUrl = "https://www.katalon.com/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void test2() throws Exception {
+        driver.get("http://localhost:3030/static/login.php");
+        driver.findElement(By.name("username")).click();
+        driver.findElement(By.name("username")).clear();
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).clear();
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("submit")).click();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
--- static/login.php
+++ static/login.php
@@ -1,33 +1,16 @@
 <?php 
     require_once("../src/main/Vendor/Gestion/Auth.php");
-<<<<<<< HEAD
 
     if(isset($_POST['submit']))
     {
         $auth = new \Vendor\Gestion\Auth();
         $error = $auth->connection($_POST['username'], $_POST['password']);
     }
-=======
-    // use Vendor\Auth;
-
-    if(isset($_POST['submit']))
-    {
-        $auth = new Auth\Auth();
-        $auth->connection($_POST['username'], $_POST['password'])."\n";
-    }
-
-    // echo "YOLOOO";
->>>>>>> origin/Calendrier_GoogleAPI
 ?>
 
 <?php $title = "log page"; ?>
 
 <!-- Start of storage -->
-<<<<<<< HEAD
-=======
-
-
->>>>>>> origin/Calendrier_GoogleAPI
 <html>
 
     <!-- Include head html -->
@@ -48,19 +31,13 @@
                 <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
                 
                     
-<<<<<<< HEAD
                     <form action="login.php" method="POST" role="form">
                         <br/>
                         <h2>Authentification</h2>
-=======
-                    <form action="" method="POST" role="form">
-                        <legend>Authentification</legend>
->>>>>>> origin/Calendrier_GoogleAPI
                     
                         <div class="form-group">
                             <label for="">Identifiant</label>
                             <input type="text" class="form-control" id="" placeholder="Identifiant" name="username">
-<<<<<<< HEAD
                         </div>
 
                         <div class="form-group">
@@ -74,11 +51,6 @@
                                 echo '<div class="alert alert-danger" role="alert">'.$error.'</div>';
                             }
                         ?>
-=======
-                            <label for="">Mot de passe</label>
-                            <input type="password" class="form-control" id="" placeholder="Mot de passe" name="password">
-                        </div>
->>>>>>> origin/Calendrier_GoogleAPI
 
                         <button type="submit" class="btn btn-default center-block" name="submit">Envoyez les croissants ! </button>
                     </form>
