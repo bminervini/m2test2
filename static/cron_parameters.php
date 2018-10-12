28ac57e874035288bfe300458df7eb03e03800c7
Kranck <frizzy.rastay@gmail.com>
2018-10-12 17:44:48 +0200

    Fixed mail sending for edu.univ-fcomte.fr mail system

--- static/cron_parameters.php
+++ static/cron_parameters.php
@@ -7,5 +7,10 @@
         echo $value;
     }
 
+    //
+    // -> /home/m2test2/public_html/preprod
+    // * * * * * * php-cli -f /home/m2test2/public_html/preprod/static/sendsimplemail.php
+
+    
 
 ?>
