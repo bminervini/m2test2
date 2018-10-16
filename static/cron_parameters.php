dafc7b9ab9c45c8fa191a21df2813ff9f6a832b4
Kranck <frizzy.rastay@gmail.com>
2018-10-16 14:04:39 +0200

    Started cron parameters page

--- static/cron_parameters.php
+++ static/cron_parameters.php
@@ -14,3 +14,28 @@
     
 
 ?>
+
+<!DOCTYPE html>
+<html>
+
+    <?php include 'head.php'?>
+
+    <body>
+
+        
+        <form action="" method="POST" role="form">
+            <legend>Form title</legend>
+        
+            <div class="form-group">
+                <label for="">label</label>
+                <input type="text" class="form-control" id="" placeholder="Input field">
+            </div>
+        
+            
+        
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        
+        
+    </body>
+</html>
