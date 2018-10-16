71dfa82cced750c76adeadfa2318bbe49cdc31d7
Awa Diallo <awa.diallo@laposte.fr>
2018-10-16 19:19:20 +0200

    update updateStatus.php

--- static/navbar.php
+++ static/navbar.php
@@ -20,8 +20,9 @@
 			<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Modifier Participation<span class="caret"></span></button>
 			
 			<ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-				<li><span class="glyphicon glyphicon-ok"></span>  Je participe</li>
-				<li><span class="glyphicon glyphicon-remove"></span>  Je ne participe pas</li>
+				<li><a href = "updateStatut.php?status=1" ><span class="glyphicon glyphicon-ok"></span>  Je participe</a></li>
+				<li><a href = "updateStatut.php?status=2"><span class="glyphicon glyphicon-remove"></span>  Je ne participe pas</a></li>
+				<!--a voire-->
 			</ul>
 		</div>
 
