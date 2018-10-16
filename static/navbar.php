5c75bcee18040793ac187c4358be5a2f9871d7be
Awa Diallo <awa.diallo@laposte.fr>
2018-10-16 15:01:56 +0200

    update navbar

--- static/navbar.php
+++ static/navbar.php
@@ -2,12 +2,12 @@
 	<a class="navbar-brand" href="/">Croissant Show</a>
 	<div class="nav navbar">
 		<div class="navbar-nav">
+			<a class="nav-item nav-link active" href="dashboard.php" style="color:white">Dashboard</a>
+			<a class="nav-item nav-link active" href="calendar.php" style="color:white">Calendrier</a>
 <?php
 	if(isset($_SESSION['admin']))
 	{
 ?>
-		<a class="nav-item nav-link active" href="dashboard.php" style="color:white">Dashboard</a>
-		<a class="nav-item nav-link active" href="calendar.php" style="color:white">Calendrier</a>
 		<a class="nav-item nav-link" href="#" style="color:white">Parametres</a>
 		<a class="nav-item nav-link" href="GestionAdhesion.php" style="color:white">Adhesions</a>
 		<a class="nav-item nav-link" href="GestionParticipants.php" style="color:white">Participants</a>
@@ -15,8 +15,6 @@
 	}else 
 	{ 
 ?>
-		<a class="nav-item nav-link active" href="dashboard.php" style="color:white">Dashboard</a>
-		<a class="nav-item nav-link active" href="calendar.php" style="color:white">Calendrier</a>
 		<a class="nav-item nav-link" href="participants.php" style="color:white">Participants</a>
 		<div class="dropdown">
 			<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Modifier Participation<span class="caret"></span></button>
