41cb8e558c2b60d4409876b6cbd44134f3fc9428
Kranck <frizzy.rastay@gmail.com>
2018-10-22 14:19:20 +0200

    Finished new mail system

--- src/main/Vendor/Mailing/MailSender.php
+++ src/main/Vendor/Mailing/MailSender.php
@@ -15,67 +15,11 @@ namespace Vendor\Mailing;
 
         public static function SendMail($sFrom , $sTo , $sSubject , $sBody)
         {
-
+            return mail($sTo , $sSubject , $sBody,
+                            "From: " . $sFrom 
+                        .   "Reply-To: m2test2.croissant.show@gmail.com"
+            );
         }
-
-    //     var $m_sLogin;  //< The login of the mail sender (from)
-    //     var $m_sPassword;   //< The password of the mail sender (from)
-
-    //     //
-    //     var $m_oMailer;
-
-    //     //Enable debug
-    //     var $m_bVerbose = false;
-
-    //     public function __construct($sLogin , $sPassword , $bVerbose = false)
-    //     {
-    //         $this->m_sLogin = $sLogin;
-    //         $this->m_sPassword = $sPassword;
-    //         $this->m_bVerbose = $bVerbose;
-
-    //         //Constructs the mailer object
-    //         $this->m_oMailer = new PHPMailer();
-    //         $this->configureSMTP();
-    //     }
-
-    //     private function configureSMTP()
-    //     {
-    //         if ($this->m_bVerbose)
-    //         {
-    //             $this->m_oMailer->SMTPDebug = 4;            //< Enable verbose
-    //         }
-    //         $this->m_oMailer->isSMTP();                 //< Use SMTP
-    //         $this->m_oMailer->Host = "smtps.univ-fcomte.fr";  //< Specify the SMTP
-    //         $this->m_oMailer->SMTPAuth = true;          //< Enable the authentication of SMTP
-    //         $this->m_oMailer->Username = $this->m_sLogin;     //< Login
-    //         $this->m_oMailer->Password = $this->m_sPassword;  //< Password
-    //         $this->m_oMailer->SMTPSecure = 'ssl';       //< ssl encyption is neede for gmail
-    //         $this->m_oMailer->Port = 465;               //< Gmail port       
-    //     }
-
-    //     public function sendMail($oMail)
-    //     {
-    //         //Configure last infos of the mail
-    //         // $this->m_oMailer->setFrom($this->m_sLogin , "Crsoissant Show Mailing System");
-    //         $this->m_oMailer->setFrom("yannis.beaux@edu.univ-fcomte.fr" , "Croissant Show Mailing System");
-
-    //         foreach ($oMail->getRecipients() as $recipient)
-    //         {
-    //             $this->m_oMailer->addAddress($recipient);
-    //         }
-            
-    //         $this->m_oMailer->Subject   = $oMail->getSubject();
-    //         $this->m_oMailer->Body      = $oMail->getBody();
-    //         $this->m_oMailer->AltBody   = $oMail->getBody();    //TOFIX: Alternative body {
-            
-    //         if (!$this->m_oMailer->send())
-    //         {
-    //             echo "<p>Erreur d'envoi du mail : <strong style='color:red;'>[" . $this->m_oMailer->ErrorInfo . "]</strong></p>";
-    //             return false;
-    //         }
-
-    //         return true;
-    //     }
-
+        
     }
 ?>
--- src/test/Vendor/Mailing/TestMailSender.php
+++ src/test/Vendor/Mailing/TestMailSender.php
@@ -8,47 +8,18 @@ namespace Vendor\Mailing\tests\units
     class MailSender extends atoum
     {
 
-        public function testBadSender()
+        public function testGoodPort()
         {
-            $sLogin = "m2test2uneadressemailquinexistepas@domainequinexistepas.com";
-            $sPassword = "unmotedepassequinexistepas";
-            $sSender = "expediteur@univ-fcomte.fr";
+            $sFrom = "m2test2.croissant.show@edu.univ-fcomte.fr";
+            $sTo = "m2test2.croissant.show@gmail.com";
+            $sSubject = "Je suis un mail généré par un test";
+            $sBody = "Voilà je corps du mail, c'est un peu nul, mais je fais ce que je peux !";
 
             $this
-                ->given($oInstance = new \Vendor\Mailing\MailSender($sLogin , $sPassword , $sSender))
-
-                ->if($oResult = $oInstance->sendMail(new \Vendor\Mailing\Mail("m2test2.croissant.show@gmail.com" , "Sujet" , "Corps")))
+                ->if($oResult = \Vendor\Mailing\MailSender::SendMail($sFrom , $sTo , $sSubject , $sBody))
                     ->boolean($oResult)
-                        ->isEqualTo(false)
+                        ->isEqualTo(true)
             ;
-        }
-
-        public function testBadCredentials()
-        {
-            $sLogin = "m2test2.croissant.show@gmail.com";
-            $sPassword = "mauvaismotdepasse";
-            $sSender = "expediteur@univ-fcomte.fr";
-
-            $this
-            ->given($oInstance = new \Vendor\Mailing\MailSender($sLogin , $sPassword , $sSender))
-            ->if($oResult = $oInstance->sendMail(new \Vendor\Mailing\Mail("m2test2.croissant.show@gmail.com" , "Sujet" , "Corps")))
-                ->boolean($oResult)
-                    ->isEqualTo(false)
-        ;
-        }
-
-        public function testSendMailSuccess()
-        {
-            $sLogin = "ybeaux";
-            $sPassword = "Cestpasfaux!120";
-            $sSender = "m2test2.croissant.show@edu.univ-fcomte.fr";
-
-            $this
-            ->given($oInstance = new \Vendor\Mailing\MailSender($sLogin , $sPassword , $sSender))
-            ->if($oResult = $oInstance->sendMail(new \Vendor\Mailing\Mail("m2test2.croissant.show@gmail.com" , "Sujet" , "Corps")))
-                ->boolean($oResult)
-                    ->isEqualTo(true)
-        ;
         }
     }
 }
