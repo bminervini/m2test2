311cdae872961ffce8270375997bc6ad7e45908f
Zarnakand <lucas.mary5@orange.fr>
2018-10-22 16:09:15 +0200

    Choix de l utilisateur elu

--- src/main/Vendor/Calendar/quickstart.php
+++ src/main/Vendor/Calendar/quickstart.php
@@ -1,69 +1,115 @@
 <?php
-//namespace Calendar;
 
-include ('./../Mailing/MailSender.php'); 
+namespace Vendor\Calendar;
+
+//require_once 'google/apiclient'; 
 
 require __DIR__ . '/vendor/autoload.php';
 
-	use \Mailing\MailSender; 
-	use \Mailing\Mail; 
+
 class quickstart 
 {
  function __construct()
      {
-		$ms = getClient(); 
+			//$ms = getClient(); 
      }
  
  
 function getClient()
 {
-	
-    $client = new Google_Client();
+    $client = new \Google_Client();
     $client->setApplicationName('Google Calendar API PHP Quickstart');
-    $client->setScopes(Google_Service_Calendar::CALENDAR);
+    $client->setScopes(\Google_Service_Calendar::CALENDAR);
     $client->setAuthConfig('credentials.json');
     $client->setAccessType('offline');
     $client->setPrompt('select_account consent');
+	$tokenPath = 'token.json';
+	//echo 'ici'; 
+	if(!file_exists($tokenPath)){
+					// Request authorization from the user.
+					$authUrl = $client->createAuthUrl();
+					echo 'Voici le lien d\'activation de l\'application : <a href=\''.$authUrl.'\' target="_blank">Ici</a>';
+					echo '
+						<form method="get">
+							<table>
+								<tr>
+									<td>
+										<input name="code_google" type="text">
+									</td>
+									<td>
+										<input type="submit" value="Envoyer">
+									</td>
+								</tr>
+							</table>
+						</form>'; 
+		
+		if(isset($_GET['code_google'])){
+			// Load previously authorized token from a file, if it exists.
+			if (file_exists($tokenPath)) {
+				$accessToken = json_decode(file_get_contents($tokenPath), true);
+				$client->setAccessToken($accessToken);
+			}
 
-    // Load previously authorized token from a file, if it exists.
-    $tokenPath = 'token.json';
-    if (file_exists($tokenPath)) {
-        $accessToken = json_decode(file_get_contents($tokenPath), true);
-        $client->setAccessToken($accessToken);
-    }
+			// If there is no previous token or it's expired.
+			if ($client->isAccessTokenExpired()) {
+				// Refresh the token if possible, else fetch a new one.
+				if ($client->getRefreshToken()) {
+					$client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+				} else {
+						$authCode = $_GET['code_google']; 
+					if($authCode != null){
+					  //  $authCode = trim(fgets(STDIN));
 
-    // If there is no previous token or it's expired.
-    if ($client->isAccessTokenExpired()) {
-        // Refresh the token if possible, else fetch a new one.
-        if ($client->getRefreshToken()) {
-            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-        } else {
-            // Request authorization from the user.
-			
-            $authUrl = $client->createAuthUrl();
-			print $authUrl; 
-			$ms = new MailSender("m2test2.croissant.show@gmail.com","pas2pitiepourlescroissants!");
-			
-			$test = $ms->sendMail(new Mail("m2test2.croissant.show@gmail.com" , "thexanoide@gmail.com" , "Ceci est un test" , "Bonjour utilisateur ! Voici le lien pour pouvoir vous inscrire à l'application Croissant Show ! :) ".$authUrl));
-            print 'Enter verification code: ';
-            $authCode = trim(fgets(STDIN));
+						// Exchange authorization code for an access token.
+						$accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+						$client->setAccessToken($accessToken);
+						// Check to see if there was an error.
+						if (array_key_exists('error', $accessToken)) {
+							throw new Exception(join(', ', $accessToken));
+						}
+					}
+				}
+				// Save the token to a file.
+				if (!file_exists(dirname($tokenPath))) {
+					mkdir(dirname($tokenPath), 0700, true);
+				}
+				file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+			}
+			return $client;
+		}
+	} else {
+		if (file_exists($tokenPath)) {
+				$accessToken = json_decode(file_get_contents($tokenPath), true);
+				$client->setAccessToken($accessToken);
+			}
 
-            // Exchange authorization code for an access token.
-            $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-            $client->setAccessToken($accessToken);
+			// If there is no previous token or it's expired.
+			if ($client->isAccessTokenExpired()) {
+				// Refresh the token if possible, else fetch a new one.
+				if ($client->getRefreshToken()) {
+					$client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+				} else {
+						$authCode = $_GET['code_google']; 
+					if($authCode != null){
+					  //  $authCode = trim(fgets(STDIN));
 
-            // Check to see if there was an error.
-            if (array_key_exists('error', $accessToken)) {
-                throw new Exception(join(', ', $accessToken));
-            }
-        }
-        // Save the token to a file.
-        if (!file_exists(dirname($tokenPath))) {
-            mkdir(dirname($tokenPath), 0700, true);
-        }
-        file_put_contents($tokenPath, json_encode($client->getAccessToken()));
-    }
-    return $client;
+						// Exchange authorization code for an access token.
+						$accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+						$client->setAccessToken($accessToken);
+						// Check to see if there was an error.
+						if (array_key_exists('error', $accessToken)) {
+							throw new Exception(join(', ', $accessToken));
+						}
+					}
+				}
+				// Save the token to a file.
+				if (!file_exists(dirname($tokenPath))) {
+					mkdir(dirname($tokenPath), 0700, true);
+				}
+				file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+			}
+			return $client;
+	}
 }
 
 /*
--- src/main/Vendor/Calendar/vendor/google/apiclient/src/Google/Client.php
+++ src/main/Vendor/Calendar/vendor/google/apiclient/src/Google/Client.php
@@ -14,6 +14,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ 
+ 
 
 use Google\Auth\ApplicationDefaultCredentials;
 use Google\Auth\Cache\MemoryCacheItemPool;
