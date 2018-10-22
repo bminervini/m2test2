208f48966215228cc99f6129dc26c21129d948ee
Kranck <frizzy.rastay@gmail.com>
2018-10-22 15:48:30 +0200

    Fixed font awesom CDN issue (taking too long in jenkins)

--- static/head.php
+++ static/head.php
@@ -6,6 +6,6 @@
     <link rel="stylesheet" href="../static/style/croissantshow.css">
 	
     <!-- Font Awesome CSS -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+    <link rel="stylesheet" href="../lib/fontawesome/fontawesome.css">
 
 </head>
